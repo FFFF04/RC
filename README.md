05f3008ad49de656d6dd002dba3d58478aa63e8a
Francisco <francisco.morao04@gmail.com>
2024-12-19 22:53:50 +0000

    Server.h

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # RC
+
+Comandos da MakeFile:
+
+make - para compliar
+
+make clean - para limpar todas as diretorias(SCORES E GAMES) bem como todos os files compilados e ficheiros .txt e .html
--- Server.h
+++ Server.h
@@ -1,6 +1,11 @@
 #ifndef SERVER_H_
 #define SERVER_H_
 
+char* start(char* arguments);
+void TRY(char* arguments, char *res_msg);
+void show_trials(char *arguments, char *res_msg);
+void scoreboard(char *res_msg);
+void quit(char* arguments, char *res_msg);
+char* debug(char* arguments);
 
 #endif
-
